0d8be6b53d39506827b78ae822f07ffb5e7d59d8
Rizwan Khan <rizwan.office1979@gmail.com>
2018-01-28 11:36:20 +0400

    create and add to admin site, create model manager, create view for list and detail, create url as well

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 # Create your models here.
+
+
+class PublishManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -17,6 +23,17 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    objects = models.Manager() # default manager
+    published = PublishManager() # our custom manager
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[
+            self.publish.year,
+            self.publish.strftime('%m'),
+            self.publish.strftime('%d'),
+            self.slug
+        ])
 
     class Meta:
         ordering = ('-publish',)
